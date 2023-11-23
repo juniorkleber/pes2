a2cee9f0967970dfec1155c07911f89287a63014
juniorkleber <kleber.alpe@gmail.com>
2023-11-23 19:59:01 -0300

    1

---
+++ test_calculadora.py
@@ -0,0 +1,27 @@
+import unittest
+from calculadora import Calculadora
+
+class TestCalculadora(unittest.TestCase):
+
+    def setUp(self):
+        self.calculadora = Calculadora()
+
+    def test_somar(self):
+        self.assertEqual(self.calculadora.somar(3, 5), 8)
+
+    def test_subtrair(self):
+        self.assertEqual(self.calculadora.subtrair(8, 3), 5)
+
+    def test_multiplicar(self):
+        self.assertEqual(self.calculadora.multiplicar(4, 6), 24)
+
+    def test_dividir(self):
+        self.assertEqual(self.calculadora.dividir(10, 2), 5)
+        self.assertEqual(self.calculadora.dividir(7, 3), 2.3333, places=4)
+
+    def test_dividir_por_zero(self):
+        with self.assertRaises(ValueError):
+            self.calculadora.dividir(5, 0)
+
+if __name__ == '__main__':
+    unittest.main()
